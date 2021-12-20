d30dcf014152ff6023170b81bbb44956e0765090
Nenad <nenad.bubalo@gmail.com>
2021-12-20 18:17:29 +0100

    Release: 20.12.21

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
-/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+// Variable
+let number = document.getElementById('number');
+let counter = 0;
+let numberCart = document.getElementById('spanContainer');
+let newCounter = 0;
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon in the mobile version */
 function myFunction() {
     let x = document.getElementById("navigation");
     let img = document.getElementById('icon-openClose');
@@ -8,5 +14,40 @@ function myFunction() {
     } else {
         x.style.display = "flex";
         img.src = "/images/icon-close.svg";
+    }
+}
+
+/* Count the number of items up */
+function countItemUp() {
+    number.innerHTML++;
+    counter++;
+}
+
+/* Count the number of items down */
+function countItemDown() {
+    if(number.innerHTML === '0') {
+        number.innerHTML = '0';
+    }
+    else {
+        number.innerHTML--;
+        counter--;
+    }
+}
+
+/* Add number of items to the cart-span-container */
+function addToCart() {
+    newCounter += counter;
+    let convert = newCounter.toString();
+    numberCart.innerHTML = convert;
+}
+
+/* open and close the basket */
+function opBasket() {
+    let cartDiv = document.getElementById('cart-div');
+    if(cartDiv.style.display === 'flex') {
+        cartDiv.style.display = 'none';
+    }
+    else {
+        cartDiv.style.display = 'flex';
     }
 }
